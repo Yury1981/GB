78195c6964f05dac46f75e98da5849db3ca4626a
Markov_Yury <umarkov@inbox.ru>
2022-08-27 22:44:15 +0300

    Added all

---
+++ Знакомство с языками программирования (семинары)/Lesson_1/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Console.WriteLine("Good evening!");
+int number = 5;
+Console.WriteLine("My number is " + number + " and its good!");
+Console.WriteLine($"My number is {number} and its good!");
+*/
+
+// Задача 1.
+/*
+Console.Write("Input integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int kvadrat = number * number;
+Console.WriteLine($"Quad of {number} is {kvadrat}");
+*/
+
+// Задача 2. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+/*
+Console.Write("Input first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2 * num2)
+{
+    Console.WriteLine($"{num1} is a quad of {num2}");
+}
+else
+{
+    Console.WriteLine($"{num1} is not a quad of {num2}");
+}
+*/
+
+// Задача 3. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+/*
+Console.Write("Input positive integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+// Задача 4. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Input positive integer number XXX: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int lastnum = num % 10;
+
+Console.WriteLine($"Last digit of {num} is: {lastnum}");
---
+++ Знакомство с языками программирования (семинары)/Practical_Task_1/Program.cs
@@ -0,0 +1,101 @@
+﻿/* 
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+/*
+Console.Write("Input first integer number : ");
+int firstnum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number : ");
+int secondnum = Convert.ToInt32(Console.ReadLine());
+
+if(firstnum == secondnum)
+{
+    Console.WriteLine("Еhe numbers are equal");
+}
+else
+{
+    if(firstnum < secondnum)
+    {
+        Console.WriteLine($"{secondnum} is largest number");
+        Console.WriteLine($"{firstnum} is lowest number");
+    }
+    else
+    {
+        Console.WriteLine($"{firstnum} is largest number");
+        Console.WriteLine($"{secondnum} is lowest number");
+    }
+}
+*/
+
+/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+/*
+Console.Write("Input first integer number : ");
+int firstnum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number : ");
+int secondnum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third integer number : ");
+int thirdnum = Convert.ToInt32(Console.ReadLine());
+
+int maxnum = firstnum;
+
+if(secondnum > maxnum)
+{
+    maxnum = secondnum;
+}
+if(thirdnum > maxnum)
+{
+    maxnum = thirdnum;
+}
+
+Console.WriteLine($"Maximum number is {maxnum}");
+*/
+
+/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+/*
+Console.Write("Input integer number : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+
+/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Input integer number : ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+
+if(num > 1)
+{
+    while(current <= num)
+    {
+        Console.Write(current + " ");
+        current += 2;
+    }
+}
+else
+{
+    Console.WriteLine("ERROR");
+}
