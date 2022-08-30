07d06e6fd20d2549d39d0ca508d57972b90b9bdd
Markov_Yury <umarkov@inbox.ru>
2022-08-30 15:04:26 +0300

    Сorrection_Lesson_4

--- Знакомство с языками программирования (семинары)/Lesson_4/Task_3.cs
+++ Знакомство с языками программирования (семинары)/Lesson_4/Task_3.cs
@@ -1,5 +1,5 @@
 // Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
-
+/*
 void PrintArray(int num)
 {
     int[] arr = new int[num];
@@ -13,3 +13,29 @@ void PrintArray(int num)
 Console.WriteLine("Input number");
 int num = Convert.ToInt32(Console.ReadLine());
 PrintArray(num);
+*/
+
+int [] CreateRandomArray(int n, int minval, int maxval)
+{
+    int [] arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new Random().Next(minval, maxval + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int [] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]}, ");
+    Console.WriteLine("\b\b]");
+}
+
+PrintArray(CreateRandomArray(8, 0, 1));
+PrintArray(CreateRandomArray(8, 0, 1));
+
+int [] newArr = CreateRandomArray(8, 0, 1);
+PrintArray(newArr);
+PrintArray(newArr);
