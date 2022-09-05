8b598a9783ccc661977fd763271f0da3164cc419
Markov_Yury <umarkov@inbox.ru>
2022-09-05 21:38:33 +0300

    End_Lesson_6

---
+++ Знакомство с языками программирования (семинары)/Lesson_6/Task_3.cs
@@ -0,0 +1,8 @@
+// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник сo сторонами такой длины.
+
+onsole.Write("Input a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input c: ");
+int c = Convert.ToInt32(Console.ReadLine());
