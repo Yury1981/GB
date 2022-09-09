a8a3a8b6345a1443814562d48560e9760dd6fbb6
Markov_Yury <umarkov@inbox.ru>
2022-09-09 17:30:43 +0300

    Correct_Lesson_7

--- Знакомство с языками программирования (семинары)/Lesson_7/Task_4.cs
+++ Знакомство с языками программирования (семинары)/Lesson_7/Task_4.cs
@@ -1,9 +1,8 @@
-﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
-
+// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
 
 int[,] CreateRandom2dArray()
 {
-    Console.Write("Input numbers of rows: ");
+     Console.Write("Input numbers of rows: ");
     int rows = Convert.ToInt32(Console.ReadLine());
     Console.Write("Input numbers of columns: ");
     int columns = Convert.ToInt32(Console.ReadLine());
@@ -20,20 +19,32 @@ int[,] CreateRandom2dArray()
     return newArray;
 }
 
+int SumMainDiagonal(int[,] array)
+{
+    int sum = 0;
+    int end = array.GetLength(0);
+    if (array.GetLength(1) < array.GetLength(0)) end = array.GetLength(1);
+    for (int i = 0; i < end; i++)
+        sum = sum + array[i,i];
+    return sum;
+}
+
 void Show2dArrray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.Write("[");
+        Console.Write("|");
         for( int j = 0; j < array.GetLength(1); j++)
         {
-            if (j < array.GetLength(1) - 1) Console.Write(array[i,j] + " ");
-            else Console.WriteLine($"{array[i,j]}]");
+            if (array[i,j] < 10) Console.Write(" 0" + array[i,j] + " |");
+            else Console.Write(" " + array[i,j] + " |");
         }
-            
+        Console.WriteLine();
     }
     Console.WriteLine();
 }
 
 int[,] myArray = CreateRandom2dArray();
 Show2dArrray(myArray);
+int sum = SumMainDiagonal(myArray);
+Console.WriteLine($"Sum Main Diagonal =: {sum}");
