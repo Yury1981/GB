26308bdb1668c0ad11db42fe670eff9324b8d2c5
Markov_Yury <umarkov@inbox.ru>
2022-09-20 01:53:04 +0300

    Lesson1

---
+++ Знакомство с языками программирования (лекции)/Practical_Task_8/Cat/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using CatMaster;
+
+namespace Cat
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine(args[0]);
+            Console.ReadKey();
+        }
+    }
+}
