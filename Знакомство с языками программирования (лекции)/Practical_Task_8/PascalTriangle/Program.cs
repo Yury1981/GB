b86dfb8488813175cd1b32fc8409932f2da8a3b9
Markov_Yury <umarkov@inbox.ru>
2022-09-25 01:54:29 +0300

    End

--- Знакомство с языками программирования (лекции)/Practical_Task_8/PascalTriangle/Program.cs
+++ Знакомство с языками программирования (лекции)/Practical_Task_8/PascalTriangle/Program.cs
@@ -6,27 +6,51 @@ namespace PascalTriangle
 {
     static public void Main()
     {
-        int i, n, c;
-        Console.WriteLine("Input rows number:");
-        string s = Console.ReadLine();
-        n = Convert.ToInt32(s);
+            Console.SetCursorPosition(30, 0);
+            Console.WriteLine("+");
 
-        for (i = 0; i < n; i++)
-        {
-            for (c = 0; c <= (n - i); c++)
+            int xa = 30, ya = 0,
+                xb = 0, yb = 30,
+                xc = 60, yc = 30;
+
+            Console.SetCursorPosition(xa, ya);
+            Console.WriteLine("+");
+
+            Console.SetCursorPosition(xb, yb);
+            Console.WriteLine("+");
+
+            Console.SetCursorPosition(xc, yc);
+            Console.WriteLine("+");
+
+            int x = xa, y = xb;
+
+            int count = 0;
+
+            while (count < 100000)
             {
-                Console.Write(" ");
+                int what = new Random().Next(0, 3);
+                if (what == 0)
+                {
+                    x = (x + xa) / 2;
+                    y = (y + ya) / 2;
+                }
+
+                if (what == 1)
+                {
+                    x = (x + xb) / 2;
+                    y = (y + yb) / 2;
+                }
+
+                if (what == 2)
+                {
+                    x = (x + xc) / 2;
+                    y = (y + yc) / 2;
+                }
+
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine("+");
+                count++;
             }
-            for (c = 0; c <= i; c++)
-            {
-                Console.Write(" ");
-                Console.Write("*");
-                    // Console.Write(Factorial.factorial(i) / (Factorial.factorial(c) * Factorial.factorial(i - c)));
-            }
-            Console.WriteLine();
-            Console.WriteLine();
-        }
-        Console.ReadKey();
     }
 }
 }
