d70027ef98e2bfa1437e8007b088560605ce23d4
Markov_Yury <umarkov@inbox.ru>
2022-10-05 21:35:58 +0300

    Lesson1

---
+++ Знакомство с языком Python (лекции)/Lesson_1/hello.py
@@ -0,0 +1,21 @@
+print('Hello world')
+value = None
+print(type(value))
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+value = 12345
+print(type(value))
+s = "'hello \'world'"
+print(s) # Вывод строки
+print(a, b, s)
+print('{2} - {1} - {0}'.format(a, b, s))
+print(f'{a} - {b} - {s}')
+f = True
+print(f)
+list = [1, 2, 3]
+print(list)
+col = ['hello', 1,2,3,4.5,True]
+print(col)
+print(a, " + ", b, " = ", a + b)
